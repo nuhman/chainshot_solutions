71ae09fe91143e79f4c2add7e0980567be29c06f
Muhammed Nuhman <nuhman7@outlook.com>
2018-11-05 07:34:37 +0530

    Combine Two Leaves
    🥇 Challenge link 
    `https://www.chainshot.com/lessons/5ba1558bd9f997cd2e33eba7/stage/5ba155e6d9f997cd2e33eba8`

--- merkle_tree/index.js
+++ merkle_tree/index.js
@@ -1,5 +1,13 @@
 class MerkleTree {
-  
+  constructor(leaves, concatHashes){
+      this.leaves = leaves;
+      this.concatHashes = concatHashes;
+  }
+
+  getRoot(){
+      return this.concatHashes(this.leaves[0], this.leaves[1]);
+  }
+
 }
 
 module.exports = MerkleTree;
