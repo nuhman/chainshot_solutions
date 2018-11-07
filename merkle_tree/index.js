97f1ea009db1812ef404d3758b916a1082114e31
Muhammed Nuhman <nuhman7@outlook.com>
2018-11-07 12:14:22 +0530

    Challenge 3: Odd Leaves
    Link: https://www.chainshot.com/lessons/5ba1558bd9f997cd2e33eba7/stage/5ba15710d9f997cd2e33ebae

--- merkle_tree/index.js
+++ merkle_tree/index.js
@@ -5,13 +5,11 @@ class MerkleTree {
   }
 
   getRoot(){
-      if(this.leaves.length <=1) return this.leaves[0];
-      for(let i=0;this.leaves.length > 1;i++){
-          console.log("time: ", i, "left: ", this.leaves[i], " | right: ", this.leaves[i + 1]);
+      for(let i=0;this.leaves.length > 1;i++){          
           const res = this.concatHashes(this.leaves[i], this.leaves[i+1]);
           this.leaves.splice(i, 1);
-          this.leaves[i] = res;
-          if(i == this.leaves.length-1) i = -1;
+          this.leaves[i] = res;          
+          if((this.leaves.length - (i+1)) == 1 || i == this.leaves.length-1) i = -1;
       }
       return this.leaves[0];
   }
