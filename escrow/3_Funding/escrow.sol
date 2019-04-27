3a60602c44160a519aae7b9ebd7883987cd3c3aa
Muhammed Nuhman <nuhman7@outlook.com>
2019-04-27 18:30:39 +0530

    Create escrow.sol

---
+++ escrow/3_Funding/escrow.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.19;
+
+contract EscrowContract {
+
+	address public arbiter;
+	address public beneficiary;
+	address public depositor;
+
+	function EscrowContract(address _arbiter, address _beneficiary) public payable {
+		arbiter = _arbiter;
+		beneficiary = _beneficiary;
+		depositor = msg.sender;
+	}
+
+}
